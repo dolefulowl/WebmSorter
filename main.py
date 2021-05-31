7b5f9cd300be82f99ff8676e452a5096ce7ce892
dolefulowl <61901844+dolefulowl@users.noreply.github.com>
2021-05-31 17:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ from webm import Webm
 
 THUMB_FOLDER = 'web/thumbnails/'
 WEBM_FOLDER = 'web/webm/'
+WEBM_AMOUNT_PATH = 'webm_amount.txt'
+
+# WEBM_AMOUNT_PATH is an optional argument
 webm = Webm(WEBM_FOLDER, THUMB_FOLDER)
 eel.init('web')
 
-# if the current  webm amount in the folder is different
-# we prepare new thumbs
-with open('webm_amount.txt', 'r', encoding='utf-8') as f:
-    last_amount = int(f.read())
-    if last_amount != webm.amount:
-        with open('webm_amount.txt', 'w', encoding='utf-8') as f:
-            f.write(str(webm.amount))
-        webm.prepare_thumbs_filenames()
 
 # this function only needs to call it from js
 @eel.expose
